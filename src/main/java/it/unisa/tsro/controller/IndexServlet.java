9806d63275ce7b5bbf6490b327d30589cccbf222
sabato-nocera <nocera.sabato@protonmail.com>
2022-06-19 12:56:07 +0200

    Creazione front-end del form per effettuare la ricerca avanzata delle repository

--- src/main/java/it/unisa/tsro/controller/IndexServlet.java
+++ src/main/java/it/unisa/tsro/controller/IndexServlet.java
@@ -2,23 +2,20 @@ package it.unisa.tsro.controller;
 
 import java.io.*;
 
-import it.unisa.tsro.model.dao.TsroDao;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "indexServlet", value = "/index-servlet")
 public class IndexServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
-
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getRequestDispatcher("./index.jsp").forward(request, response);
     }
 
-    public void destroy() {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
     }
 }
